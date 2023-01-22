bb634a5dbfc2a9a95435785a4c94fe3107fb91ec
ollo69 <oliveroandrea@libero.it>
2023-01-22 17:14:53 +0100

    Replace OptionFlow with SchemaOptionsFlowHandler

--- custom_components/asuswrt_custom/config_flow.py
+++ custom_components/asuswrt_custom/config_flow.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import logging
 import os
 import socket
-from typing import Any
+from typing import Any, cast
 
 import voluptuous as vol
 
@@ -13,7 +13,7 @@ from homeassistant.components.device_tracker.const import (
     CONF_CONSIDER_HOME,
     DEFAULT_CONSIDER_HOME,
 )
-from homeassistant.config_entries import ConfigEntry, ConfigFlow, OptionsFlow
+from homeassistant.config_entries import ConfigEntry, ConfigFlow
 from homeassistant.const import (
     CONF_BASE,
     CONF_HOST,
@@ -27,6 +27,11 @@ from homeassistant.core import callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaCommonFlowHandler,
+    SchemaFlowFormStep,
+    SchemaOptionsFlowHandler,
+)
 
 from .bridge import AsusWrtBridge
 from .const import (
@@ -59,6 +64,51 @@ RESULT_SUCCESS = "success"
 RESULT_UNKNOWN = "unknown"
 
 _LOGGER = logging.getLogger(__name__)
+
+LEGACY_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_MODE, default=MODE_ROUTER): vol.In(
+            {MODE_ROUTER: "Router", MODE_AP: "Access Point"}
+        ),
+    }
+)
+
+OPTIONS_SCHEMA = vol.Schema(
+    {
+        vol.Optional(
+            CONF_CONSIDER_HOME, default=DEFAULT_CONSIDER_HOME.total_seconds()
+        ): vol.All(vol.Coerce(int), vol.Clamp(min=0, max=900)),
+        vol.Optional(CONF_TRACK_UNKNOWN, default=DEFAULT_TRACK_UNKNOWN): bool,
+    }
+)
+
+
+async def get_options_schema(handler: SchemaCommonFlowHandler) -> vol.Schema:
+    """Get options schema."""
+    options_flow: SchemaOptionsFlowHandler
+    options_flow = cast(SchemaOptionsFlowHandler, handler.parent_handler)
+    used_protocol = options_flow.config_entry.data[CONF_PROTOCOL]
+    if used_protocol in [PROTOCOL_SSH, PROTOCOL_TELNET]:
+        data_schema = OPTIONS_SCHEMA.extend(
+            {
+                vol.Required(CONF_INTERFACE, default=DEFAULT_INTERFACE): str,
+                vol.Required(CONF_DNSMASQ, default=DEFAULT_DNSMASQ): str,
+            }
+        )
+        if options_flow.config_entry.data[CONF_MODE] == MODE_AP:
+            return data_schema.extend(
+                {
+                    vol.Optional(CONF_REQUIRE_IP, default=True): bool,
+                }
+            )
+        return data_schema
+
+    return OPTIONS_SCHEMA
+
+
+OPTIONS_FLOW = {
+    "init": SchemaFlowFormStep(get_options_schema),
+}
 
 
 def _is_file(value: str) -> bool:
@@ -94,12 +144,12 @@ class AsusWrtFlowHandler(ConfigFlow, domain=DOMAIN):
         if self.show_advanced_options:
             add_schema = {
                 vol.Optional(CONF_PASSWORD): str,
-                vol.Optional(CONF_SSH_KEY): str,
                 vol.Required(
                     CONF_PROTOCOL,
                     default=user_input.get(CONF_PROTOCOL, PROTOCOL_HTTPS),
                 ): vol.In(ALLOWED_PROTOCOL),
                 vol.Optional(CONF_PORT): cv.port,
+                vol.Optional(CONF_SSH_KEY): str,
             }
         else:
             add_schema = {vol.Required(CONF_PASSWORD): str}
@@ -115,18 +165,6 @@ class AsusWrtFlowHandler(ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(schema),
             errors={CONF_BASE: error} if error else None,
         )
-
-    @callback
-    def _show_legacy_form(self) -> FlowResult:
-        """Show the setup form to the user for legacy options."""
-        data_schema = vol.Schema(
-            {
-                vol.Required(CONF_MODE, default=MODE_ROUTER): vol.In(
-                    {MODE_ROUTER: "Router", MODE_AP: "Access Point"}
-                ),
-            }
-        )
-        return self.async_show_form(step_id="legacy", data_schema=data_schema)
 
     async def _async_check_connection(
         self, user_input: dict[str, Any]
@@ -243,7 +281,7 @@ class AsusWrtFlowHandler(ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle a flow for legacy settings."""
         if user_input is None:
-            return self._show_legacy_form()
+            return self.async_show_form(step_id="legacy", data_schema=LEGACY_SCHEMA)
 
         self._config_data.update(user_input)
         return await self._async_save_entry()
@@ -268,70 +306,6 @@ class AsusWrtFlowHandler(ConfigFlow, domain=DOMAIN):
 
     @staticmethod
     @callback
-    def async_get_options_flow(config_entry: ConfigEntry) -> OptionsFlow:
-        """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
-
-
-class OptionsFlowHandler(OptionsFlow):
-    """Handle a option flow for AsusWrt."""
-
-    def __init__(self, config_entry: ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Handle options flow."""
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-        used_protocol = self.config_entry.data[CONF_PROTOCOL]
-
-        data_schema = vol.Schema(
-            {
-                vol.Optional(
-                    CONF_CONSIDER_HOME,
-                    default=self.config_entry.options.get(
-                        CONF_CONSIDER_HOME, DEFAULT_CONSIDER_HOME.total_seconds()
-                    ),
-                ): vol.All(vol.Coerce(int), vol.Clamp(min=0, max=900)),
-                vol.Optional(
-                    CONF_TRACK_UNKNOWN,
-                    default=self.config_entry.options.get(
-                        CONF_TRACK_UNKNOWN, DEFAULT_TRACK_UNKNOWN
-                    ),
-                ): bool,
-            }
-        )
-
-        if used_protocol in [PROTOCOL_SSH, PROTOCOL_TELNET]:
-            data_schema = data_schema.extend(
-                {
-                    vol.Required(
-                        CONF_INTERFACE,
-                        default=self.config_entry.options.get(
-                            CONF_INTERFACE, DEFAULT_INTERFACE
-                        ),
-                    ): str,
-                    vol.Required(
-                        CONF_DNSMASQ,
-                        default=self.config_entry.options.get(
-                            CONF_DNSMASQ, DEFAULT_DNSMASQ
-                        ),
-                    ): str,
-                }
-            )
-            if self.config_entry.data[CONF_MODE] == MODE_AP:
-                data_schema = data_schema.extend(
-                    {
-                        vol.Optional(
-                            CONF_REQUIRE_IP,
-                            default=self.config_entry.options.get(
-                                CONF_REQUIRE_IP, True
-                            ),
-                        ): bool,
-                    }
-                )
-
-        return self.async_show_form(step_id="init", data_schema=data_schema)
+    def async_get_options_flow(config_entry: ConfigEntry) -> SchemaOptionsFlowHandler:
+        """Get options flow for this handler."""
+        return SchemaOptionsFlowHandler(config_entry, OPTIONS_FLOW)
